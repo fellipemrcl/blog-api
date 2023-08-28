c72e0476d827ca3614a40590c279ec78a15a2ab0
Fellipe Marcel <fmrclms@gmail.com>
2023-08-28 17:15:35 -0300

    feat: created PostCategory model

---
+++ src/models/PostCategory.js
@@ -0,0 +1,47 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define(
+    "PostCategory",
+    {
+      postId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "blog_posts",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+        primaryKey: true,
+      },
+      categoryId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "categories",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+        primaryKey: true,
+      },
+    },
+    {
+      timestamps: false,
+      tableName: "posts_categories",
+      underscored: true,
+    }
+  );
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: "categories",
+      through: PostCategory,
+      foreignKey: "post_id",
+      otherKey: "category_id",
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+      as: "blogPosts",
+      through: PostCategory,
+      foreignKey: "category_id",
+      otherKey: "post_id",
+    });
+  };
+
+  return PostCategory;
+};
