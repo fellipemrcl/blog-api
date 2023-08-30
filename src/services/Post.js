f03d24e70d0bbd3e680f88ca2790597327f4dccb
Fellipe Marcel <fmrclms@gmail.com>
2023-08-30 10:14:05 -0300

    fix: fixed lint issues
    refactor: exported 'checkCategoryIds' function

--- src/services/Post.js
+++ src/services/Post.js
@@ -1,19 +1,6 @@
 const { BlogPost, Category, PostCategory, User } = require('../models');
+const { checkCategoryIds } = require('./validations/inputValidations');
 const { postSchema } = require('./validations/schema');
-
-const checkCategoryIds = async (categoryIds) => {
-  const arrOfCategoriesPromise = categoryIds.map((categoryId) =>
-    Category.findByPk(categoryId));
-  const checkForNullCategories = (
-    await Promise.all(arrOfCategoriesPromise)
-  ).some((category) => category === null);
-  if (checkForNullCategories) {
-    return {
-      status: 'INVALID_VALUE',
-      data: { message: 'one or more "categoryIds" not found' },
-    };
-  }
-};
 
 const createPost = async ({ title, content, categoryIds, userId }) => {
   const { error } = postSchema.validate({ title, content, categoryIds, userId });
--- src/services/validations/inputValidations.js
+++ src/services/validations/inputValidations.js
@@ -1,4 +1,5 @@
 const { userSchema, categorySchema, postSchema } = require('./schema');
+const { Category } = require('../../models');
 
 const userValidation = (user) => {
   const { error } = userSchema.validate(user);
@@ -30,8 +31,23 @@ const validatePostCategory = (post) => {
    }
 };
 
+const checkCategoryIds = async (categoryIds) => {
+  const arrOfCategoriesPromise = categoryIds.map((categoryId) =>
+    Category.findByPk(categoryId));
+  const checkForNullCategories = (
+    await Promise.all(arrOfCategoriesPromise)
+  ).some((category) => category === null);
+  if (checkForNullCategories) {
+    return {
+      status: 'INVALID_VALUE',
+      data: { message: 'one or more "categoryIds" not found' },
+    };
+  }
+};
+
 module.exports = {
   userValidation,
   validateNameCategory,
   validatePostCategory,
+  checkCategoryIds,
 };
